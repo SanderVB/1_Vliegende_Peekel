703cf627d8630986733bde8c7710cffee26eab2b
Sander van Beek <vanbeekster@gmail.com>
2019-01-09 18:58:33 +0100

    update

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    int currentSceneIndex;
+
+    private void Awake()
+    {
+        int levelManagerCount = FindObjectsOfType<LevelManager>().Length;
+        if (levelManagerCount > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+    }
+    private void Start()
+    {
+        StartCoroutine(UpdateSceneIndex());
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+        StartCoroutine(UpdateSceneIndex());
+    }
+
+    public void LoadPreviousLevel()
+    {
+        if (currentSceneIndex < 1)
+            currentSceneIndex = 1;
+        SceneManager.LoadScene(currentSceneIndex - 1);
+        StartCoroutine(UpdateSceneIndex());
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(currentSceneIndex);
+        StartCoroutine(UpdateSceneIndex());
+    }
+
+    IEnumerator UpdateSceneIndex()
+    {
+        yield return 0;
+        if(currentSceneIndex<SceneManager.sceneCount) //%
+            currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,45 +2,111 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using UnityEngine.SceneManagement;
 
 public class Movement : MonoBehaviour
 {
-    AudioSource moveSound;
+    AudioSource myAudioSource;
     Rigidbody myRigidBody;
+
     [SerializeField] float thrustSpeed = 100f;
     [SerializeField] float rotationThrust = 100f;
+    [SerializeField] float waitTime = 1f;
+    [SerializeField] AudioClip engineSound;
+    [SerializeField] AudioClip deathSound;
+    [SerializeField] AudioClip winSound;
+    [SerializeField] ParticleSystem engineEffect;
+    [SerializeField] ParticleSystem deathEffect;
+    [SerializeField] ParticleSystem winEffect;
+    [SerializeField] GameObject engineLight; //betere methode zoeken
+
+    enum State { Alive, Dying, Transcending};
+    State state = State.Alive;
+
+    public bool collisionSwitch = false;
 
 	// Use this for initialization
 	void Start ()
     {
         myRigidBody = GetComponent<Rigidbody>();
-        moveSound = GetComponent<AudioSource>();
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-        Thrust();
-        Rotate();
-	}
-    private void Thrust()
-    {
-        //float thrustThisFrame = thrustSpeed * Time.deltaTime;
-        if (Input.GetKey(KeyCode.Space))
-        {
-            //thrust
-            //myRigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
-            myRigidBody.AddRelativeForce(Vector3.up * thrustSpeed);
-            if (!moveSound.isPlaying) //stops layering
-            {
-                moveSound.Play();
-            }
-        }
-        else
-            moveSound.Stop();
+        myAudioSource = GetComponent<AudioSource>();
     }
 
-    private void Rotate()
+    // Update is called once per frame
+    void Update ()
+    {
+        if (state == State.Alive)
+        {
+            RespondToThrustInput();
+            RespondToRotateInput();
+        }
+        if(Debug.isDebugBuild)
+            DebugMode();
+	}
+
+    private void DebugMode()
+    {
+      bool keyPressed = false;
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            if (!keyPressed)
+            {
+                keyPressed = true;
+                LoadNextLevel();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (!keyPressed)
+            {
+                keyPressed = true;
+                LoadPreviousLevel();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (!keyPressed)
+            {
+                keyPressed = true;
+                collisionSwitch = !collisionSwitch;
+            }
+        }
+
+        if(Input.GetKeyUp(KeyCode.C) || Input.GetKeyUp(KeyCode.L) || Input.GetKeyUp(KeyCode.P))
+        {
+            keyPressed = false; 
+        }
+    }
+
+    private void RespondToThrustInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Thrust();
+        }
+        else
+        {
+            myAudioSource.Stop();
+            engineEffect.Stop();
+            engineLight.SetActive(false);
+        }
+    }
+
+        private void Thrust()
+    {
+        engineLight.SetActive(true);
+        float thrustThisFrame = thrustSpeed * Time.deltaTime;
+        myRigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
+        //myRigidBody.AddRelativeForce(Vector3.up * thrustSpeed);
+        if (!myAudioSource.isPlaying) //stops layering
+        {
+            myAudioSource.PlayOneShot(engineSound);
+            engineEffect.Play();
+        }
+    }
+
+    private void RespondToRotateInput()
     {
 
         float rotationThisFrame = rotationThrust * Time.deltaTime;
@@ -58,5 +124,76 @@ public class Movement : MonoBehaviour
         }
         myRigidBody.freezeRotation = false; //physics take over again
 
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        /*if (collision.gameObject.tag == "Friendly")
+        {
+
+        }
+        else
+            Destroy(gameObject);*/
+
+        if (state != State.Alive) return; //don't handle collision if dead
+
+        if (!collisionSwitch)
+        {
+            switch (collision.gameObject.tag)
+            {
+                case "Friendly":
+                    //stay alive
+                    break;
+
+                case "Fuel":
+                    //refuel 
+                    break;
+
+                case "Finish":
+                    //load next level
+                    StartWinSequence();
+                    break;
+
+                default:
+                    //handle death
+                    StartDeathSequence();
+                    break;
+            }
+        }
+    }
+
+
+    private void StartWinSequence()
+    {
+        state = State.Transcending;
+        myAudioSource.Stop();
+        winEffect.Play();
+        myAudioSource.PlayOneShot(winSound);
+        Invoke("LoadNextLevel", waitTime);
+    }
+
+    private void StartDeathSequence()
+    {
+        state = State.Dying;
+        myAudioSource.Stop();
+        deathEffect.Play();
+        myAudioSource.PlayOneShot(deathSound);
+        Invoke("RestartLevel", waitTime);
+    }
+
+    private void LoadNextLevel() 
+    {
+        FindObjectOfType<LevelManager>().LoadNextLevel();
+    }
+
+    private void LoadPreviousLevel()
+    {
+        FindObjectOfType<LevelManager>().LoadPreviousLevel();
+    }
+
+
+    private void RestartLevel()
+    {
+        FindObjectOfType<LevelManager>().RestartLevel();
     }
 }
